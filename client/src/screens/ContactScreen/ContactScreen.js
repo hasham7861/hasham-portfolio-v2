8d09684009f654e9fcaeec6bd1a80cd2f8c12660
Hasham Alam <hasham.alam@hotmail.com>
2019-02-03 12:53:32 -0500

    Created a contact page with a form

--- client/src/screens/ContactScreen/ContactScreen.js
+++ client/src/screens/ContactScreen/ContactScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './ContactScreen.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import App from '../../App';
+
 class ContactScreen extends Component {
 
   constructor() {
@@ -10,9 +13,26 @@ class ContactScreen extends Component {
 
   render() {
     return (
-      <div className="ContactScreen">
-        <h1>Contact Me</h1>
-      </div>
+      <Router>
+        <div>
+          <Route path="/contact" render={props =>
+            <div className="ContactScreen">
+              <Link to='/'><h5>◀ Go Back To Home</h5></Link>
+              <h1>Contact Me</h1>
+              <form>
+                <input type="text" name="email" placeholder="Enter in your email.."/>
+                <textarea name="message" placeholder="Enter in your message.. "></textarea>
+                <button>Send Message</button>
+              </form>
+              <div class="contactBar">
+
+              </div>
+            </div>}
+          />
+          <Route exact path='/' component={App}/>
+
+        </div>
+        </Router>
     );
   }
 }
