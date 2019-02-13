5f59d83434dcfe62671ff475d85ebadb17acd2e9
Hasham Alam <hasham.alam@hotmail.com>
2019-02-12 22:15:33 -0500

    Touch event handler added for peeking portfolio

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Portfolio.css';
 
-const Portfolio = (props) => {
+// The following touch events only work with touch devices
+const handleTouchStart = (touchStartEvent) => {
+  console.log("Touch Start on peek portfolio");
+}
 
+const handleTouchMove = (touchMoveEvent) => {
+  console.log("Touch Move on peek portfolio");
+}
+
+const handleTouchEnd = () => {
+  console.log("Touch End on peek portfolio");
+}
+
+const Portfolio = (props) => {
   return (
-    <div className="portfolio">
+    <div className="portfolio"
+      onTouchStart={touchStartEvent => handleTouchStart(touchStartEvent)}
+      onTouchMove={touchMoveEvent => handleTouchMove(touchMoveEvent)}
+      onTouchEnd={()=>handleTouchEnd()}>
       <h2 className="upArrow">▲</h2>
       <h4>Swipe Up To Peek Portfolio</h4>
     </div>
 )};
 
-  export default Portfolio;
+export default Portfolio;
