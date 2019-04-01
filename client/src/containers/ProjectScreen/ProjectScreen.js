0fe8cf78daa6025be0bc5ceb7b3187143187c1d9
Hasham Alam <hasham.alam@hotmail.com>
2019-04-01 13:44:16 -0400

    Added cards inside projectScreen

--- client/src/containers/ProjectScreen/ProjectScreen.js
+++ client/src/containers/ProjectScreen/ProjectScreen.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import ProjectNavbar from '../../components/ProjectNavbar/ProjectNavbar';
+import './ProjectScreen.scss';
 
 class ProjectScreen extends Component {
     
     projectName = "";
+    projectDescription = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'.";
+    projectSkillsUsed = "PHP, Javscript";
+    projectContributors = "Hasham Alam, Jimmy Yang";
     constructor({props, match}){
         super(props);
         this.projectName = match.params.id;
@@ -12,8 +16,22 @@ class ProjectScreen extends Component {
     render(){
         return (
             <>
-            <ProjectNavbar projectRoute={this.projectName}/>
-            <div>{this.projectName}</div>
+                <ProjectNavbar projectRoute={this.projectName}/>
+                <div className="ProjectScreen">
+                    <h1>{this.projectName}</h1>
+                    <section className="Description">
+                        <h3>Description</h3>
+                        <p>{this.projectDescription}</p>
+                    </section>
+                    <section className="SkillsUsed">
+                        <h3>SkillsUsed</h3>
+                        <p>{this.projectSkillsUsed}</p>
+                    </section>
+                    <section className="Contributors">
+                        <h3>Contributors</h3>
+                        <p>{this.projectContributors}</p>
+                    </section>
+                </div>
             </>
         )
     }
