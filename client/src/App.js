b88b4ae79b43d7f5eec7fbb9d8d634d0d32567b7
Hasham Alam <hasham.alam@hotmail.com>
2019-03-31 20:27:42 -0400

    Added InvalidRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import ContactScreen from './containers/ContactScreen/ContactScreen';
 import Home from './Home';
 import './components/Heading/Heading.css';
+import InvalidRoute from './components/InvalidRoute/InvalidRoute';
 
 class App extends Component {
  
@@ -17,7 +18,8 @@ class App extends Component {
             <Route path="/" exact component={Home}/>
             {/* TODO: Change the contact page into more detailed pages */}
             <Route path='/contact' exact component={ContactScreen}/>
-            <Router path="/project/:id" exact/>
+            <Route path="/project/:id" exact/>
+            <Route component={InvalidRoute}/> 
           </Switch>
         </Router>
 
---
+++ client/src/components/InvalidRoute/InvalidRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvalidRoute = () => {
+    return (
+        <div>
+            <h1 style={{color:'white', marginTop:0, paddingTop:'20px'}}>Invalid Route</h1>
+        </div>
+    )
+}
+
+export default InvalidRoute;
