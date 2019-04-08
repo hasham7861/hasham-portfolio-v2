7ad5999a7fd2e2d0e00296fc57f6ff809ff6db37
Hasham Alam <hasham.alam@hotmail.com>
2019-04-08 00:49:27 -0400

    Using ESLint & Prettier inside this project

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 import ContactScreen from './containers/ContactScreen/ContactScreen';
@@ -9,29 +9,23 @@ import InvalidRoute from './components/InvalidRoute/InvalidRoute';
 import ProjectScreen from './containers/ProjectScreen/ProjectScreen';
 import AboutScreen from './containers/AboutScreen/AboutScreen';
 
-
-
 class App extends Component {
- 
   render() {
-    
     return (
       <div className="App">
         <Router>
           <Switch>
-            <Route path="/" exact component={Home}/>
+            <Route path="/" exact component={Home} />
             {/* TODO: Change the contact page into more detailed pages */}
-            <Route path="/about" exact component={AboutScreen}/>
-            <Route path='/contact' exact component={ContactScreen}/>
-            <Route path="/project/:id/" exact component={ProjectScreen}/>
-            <Route component={InvalidRoute}/> 
+            <Route path="/about" exact component={AboutScreen} />
+            <Route path="/contact" exact component={ContactScreen} />
+            <Route path="/project/:id/" exact component={ProjectScreen} />
+            <Route component={InvalidRoute} />
           </Switch>
         </Router>
-
       </div>
     );
   }
 }
-
 
 export default App;
