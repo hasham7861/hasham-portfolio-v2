33345787b14649bda187b3cf9fc6de9a285a8f28
Hasham Alam <hasham.alam@hotmail.com>
2019-04-20 07:50:27 -0400

    updated deploy script and del registerworker

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
@@ -13,8 +12,3 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
