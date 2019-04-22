6bc9cc52b815d3f4a649b2592826ffceea65ca4e
Hasham Alam <hasham.alam@hotmail.com>
2019-04-22 11:59:37 -0400

    added icon to server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors'); // Allow Access-Control-Allow-Origin
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan'); // use a logger for the the request being made to app
 var bodyParser = require('body-parser'); // Helps parse HTTP post request
 
@@ -13,14 +14,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors()); // Adding cross orgin header to http request
+app.use(bodyParser.json()); // Help parse parameters from http request
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors()); // Adding cross orgin header to http request
-app.use(bodyParser.json()); // Help parse parameters from http request
+
 
 // All the routes 
 app.use('/', indexRouter);
