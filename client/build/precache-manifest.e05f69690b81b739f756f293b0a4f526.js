5784467ac29febaf122827911b73c5c6b0fd8a2e
Hasham Alam <hasham@MacBook-Pro.local>
2020-08-16 20:57:55 -0400

    add build to repo

---
+++ client/build/precache-manifest.e05f69690b81b739f756f293b0a4f526.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f8bc5a0e7cf2382cc14a0f16e78273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93636616db34a0a17567",
+    "url": "/static/css/main.687859e8.chunk.css"
+  },
+  {
+    "revision": "a1b30ca12ebeda824111",
+    "url": "/static/js/2.511a01f2.chunk.js"
+  },
+  {
+    "revision": "93636616db34a0a17567",
+    "url": "/static/js/main.6b60cc54.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "accefda391d49aa3294d535a3433fea4",
+    "url": "/static/media/AboutIcon.accefda3.svg"
+  },
+  {
+    "revision": "5e8bf2d5e3b8a12d5c73b0206e6ba1c5",
+    "url": "/static/media/AdobeXDIcon.5e8bf2d5.svg"
+  },
+  {
+    "revision": "d0905453109497a8cca35de5d6c10a24",
+    "url": "/static/media/GithubIcon.d0905453.svg"
+  },
+  {
+    "revision": "ef96f477eddaeee4bad3afa685cc3abc",
+    "url": "/static/media/HomeIcon.ef96f477.svg"
+  },
+  {
+    "revision": "c69727e24cea331422116e5b6e2c2987",
+    "url": "/static/media/JSIcon.c69727e2.svg"
+  },
+  {
+    "revision": "9afa1353142b33cf2e2c0d965ee92d8a",
+    "url": "/static/media/LinkedinIcon.9afa1353.svg"
+  },
+  {
+    "revision": "dddc4114615a6eb9d617d975c7aa513f",
+    "url": "/static/media/MyFace.dddc4114.jpg"
+  },
+  {
+    "revision": "da15bf6d322d1bda7797374be8248f92",
+    "url": "/static/media/MySqlIcon.da15bf6d.svg"
+  },
+  {
+    "revision": "e5800bf2b5f428ec599acb5b48406dc4",
+    "url": "/static/media/NodeIcon.e5800bf2.svg"
+  },
+  {
+    "revision": "de948ef5c678fb0b3fd8b60c596dd7b5",
+    "url": "/static/media/PortfolioIcon.de948ef5.svg"
+  },
+  {
+    "revision": "6e32ccc45d6264f93c2486d7e90505f3",
+    "url": "/static/media/ReactIcon.6e32ccc4.svg"
+  },
+  {
+    "revision": "a438a8d806dff56895dbd94be24c361f",
+    "url": "/static/media/ReduxIcon.a438a8d8.svg"
+  },
+  {
+    "revision": "4843dabb7e3e55c2c2e776c31a606f6a",
+    "url": "/static/media/TwitterIcon.4843dabb.svg"
+  },
+  {
+    "revision": "19da854b3e24cd2e7dc2e3f9df5b1510",
+    "url": "/static/media/YoutubeIcon.19da854b.svg"
+  },
+  {
+    "revision": "5dddcc1dd0245bb41899676cfba1fe7a",
+    "url": "/static/media/colony.5dddcc1d.svg"
+  },
+  {
+    "revision": "2bee6d4e1aa43899104e33d0c4fe94a9",
+    "url": "/static/media/hand.2bee6d4e.svg"
+  },
+  {
+    "revision": "5aae553739252c3af8c9fb455b030dc0",
+    "url": "/static/media/layers.5aae5537.svg"
+  },
+  {
+    "revision": "1ad2ff0f934f820fb22a67d7b3bcb7b2",
+    "url": "/static/media/network.1ad2ff0f.svg"
+  },
+  {
+    "revision": "214cae9ade2a5538d419ddf7c37be09c",
+    "url": "/static/media/project-management.214cae9a.svg"
+  },
+  {
+    "revision": "fbecda654713aa0903fe0327f58ffd00",
+    "url": "/static/media/start-up.fbecda65.svg"
+  }
+]);
