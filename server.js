ee1894ad4d3e2d3de6db8cc9d31e8fe6c494a420
Hasham <hasham.alam@hotmail.com>
2021-05-22 19:18:33 -0400

    no need for a node script

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 8000
-
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-app.listen(port, () => console.log(`Server on ${port}`))
